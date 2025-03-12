0fa97e65fb8be3771a130d1b7d8f6cf9f537aeb9
UzairBinAsif <shaikhuzairasif14@gmail.com>
2025-03-12 23:19:56 +0500

    add api URL

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ if st.button('Generate Money'):
 
 def fetch_side_hustles():
     try:
-        res = requests.get('http://127.0.0.1:8000/side_hustles?apiKey=181278')
+        res = requests.get('https://simple-api-python.vercel.app/side_hustles?apiKey=181278')
         if res.status_code == 200:
             hustles = res.json()
             return hustles['side_hustle']
@@ -35,7 +35,7 @@ if st.button('Generate Hustle'):
 
 def fetch_money_quotes():
     try:
-        res = requests.get('http://127.0.0.1:8000/money_quotes?apiKey=181278')
+        res = requests.get('https://simple-api-python.vercel.app/money_quotes?apiKey=181278')
         if res.status_code == 200:
             quotes = res.json()
             return quotes['money_quote']
